91b5742e71620dc4df7d2d21808089fe44949a8e
Mario Mucalo <mucalo.mario@gmail.com>
2025-12-01 11:32:00 +0100

    Task 1 solved

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2025.Tasks;
+
+Console.WriteLine("AoC 2025");
+
+var dateTimeStart = DateTime.Now;
+double s = 0;
+double ms = 0;
+
+// Task 1.1
+Console.Write("01.1, solution: " + Task01.Part1());
+ms = (DateTime.Now - dateTimeStart).TotalMilliseconds - s;
+Console.WriteLine(" in " + ms + " ms.");
+s += ms;
+
+// Task 1.2
+Console.Write("01.2, solution: " + Task01.Part2());
+ms = (DateTime.Now - dateTimeStart).TotalMilliseconds - s;
+Console.WriteLine(" in " + ms + " ms.");
+s += ms;
+
+
---
+++ Tasks/Task01.cs
@@ -0,0 +1,65 @@
+﻿namespace AdventOfCode2025.Tasks
+{
+    public static class Task01
+    {
+        public static int Part1()
+        {
+            var lines = File.ReadAllLines("../../../Inputs/01.1.txt");
+            int s = 0;
+            int position = 50;
+            foreach (var line in lines)
+            {
+                var direction = line[0];
+                var value = int.Parse(line[1..]);
+
+                if (direction == 'L')
+                {
+                    position -= value;
+                }
+                else
+                {
+                    position += value;
+                }
+
+                position %= 100;
+
+                if (position == 0) s++;
+            }
+
+            return s;
+        }
+
+        public static int Part2()
+        {
+            var lines = File.ReadAllLines("../../../Inputs/01.1.txt");
+            int s = 0;
+            int position = 50;
+            int oldPosition = 50;
+            foreach (var line in lines)
+            {
+                oldPosition = position;
+                var direction = line[0];
+                var value = int.Parse(line[1..]);
+
+                if (direction == 'L')
+                {
+                    position -= value;
+                }
+                else
+                {
+                    position += value;
+                }
+
+                int numOfTimesPassedZero = Math.Abs(position / 100);
+                if ((oldPosition < 0 && position >= 0) || (oldPosition > 0 && position <= 0))
+                {
+                    numOfTimesPassedZero++;
+                }
+                s += numOfTimesPassedZero;
+                position %= 100;
+            }
+
+            return s;
+        }
+    }
+}

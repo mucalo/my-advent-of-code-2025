a8f45d9331a55dc213a92a0f30b35306b4580a01
Mario Mucalo <mucalo.mario@gmail.com>
2025-12-02 14:11:41 +0100

    Task 2 bruteforced :)

--- Program.cs
+++ Program.cs
@@ -6,16 +6,27 @@ var dateTimeStart = DateTime.Now;
 double s = 0;
 double ms = 0;
 
-// Task 1.1
-Console.Write("01.1, solution: " + Task01.Part1());
+//// Task 1.1
+//Console.Write("01.1, solution: " + Task01.Part1());
+//ms = (DateTime.Now - dateTimeStart).TotalMilliseconds - s;
+//Console.WriteLine(" in " + ms + " ms.");
+//s += ms;
+
+//// Task 1.2
+//Console.Write("01.2, solution: " + Task01.Part2());
+//ms = (DateTime.Now - dateTimeStart).TotalMilliseconds - s;
+//Console.WriteLine(" in " + ms + " ms.");
+//s += ms;
+
+//// Task 2.1
+//Console.Write("02.1, solution: " + Task02.Part1());
+//ms = (DateTime.Now - dateTimeStart).TotalMilliseconds - s;
+//Console.WriteLine(" in " + ms + " ms.");
+//s += ms;
+
+// Task 2.2
+Console.Write("02.2, solution: " + Task02.Part2());
 ms = (DateTime.Now - dateTimeStart).TotalMilliseconds - s;
 Console.WriteLine(" in " + ms + " ms.");
 s += ms;
-
-// Task 1.2
-Console.Write("01.2, solution: " + Task01.Part2());
-ms = (DateTime.Now - dateTimeStart).TotalMilliseconds - s;
-Console.WriteLine(" in " + ms + " ms.");
-s += ms;
-
 
---
+++ Tasks/Task02.cs
@@ -0,0 +1,76 @@
+﻿
+namespace AdventOfCode2025.Tasks
+{
+    public static class Task02
+    {
+        private static bool IsValid(string input)
+        {
+            if (input.Length % 2 != 0) return true;
+            string part1 = input.Substring(0, input.Length / 2);
+            string part2 = input.Substring(input.Length / 2);
+            if (part1 == part2) return false; else return true;
+        }
+
+        private static bool IsRecursivelyInvalid(string input, string part)
+        {
+            if (input == "") return true;
+            if (input.StartsWith(part))
+            {
+                return IsRecursivelyInvalid(input.Substring(part.Length), part);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static long Part1()
+        {
+            long s = 0;
+            var lines = File.ReadAllText("../../../Inputs/02.1.txt").Split(",");
+            foreach (var line in lines)
+            {
+                var parts = line.Split('-');
+                long start = long.Parse(parts[0]);
+                long end = long.Parse(parts[1]);
+                long i = start;
+                while (i <= end)
+                {
+                    if (!IsValid(i.ToString())) s += i;
+
+                    i++;
+                }
+
+            }
+            return s;
+        }
+
+        public static long Part2()
+        {
+            long s = 0;
+            var lines = File.ReadAllText("../../../Inputs/02.1.txt").Split(",");
+            foreach (var line in lines)
+            {
+                var parts = line.Split('-');
+                long start = long.Parse(parts[0]);
+                long end = long.Parse(parts[1]);
+                long i = start;
+                while (i <= end)
+                {
+                    for (int len = 1; len <= i.ToString().Length / 2; len++)
+                    {
+                        string part = i.ToString().Substring(0, len);
+                        if (IsRecursivelyInvalid(i.ToString(), part))
+                        {
+                            s += i;
+                            break;
+                        }
+                    }
+                    i++;
+                }
+
+            }
+            return s;
+        }
+    }
+}
